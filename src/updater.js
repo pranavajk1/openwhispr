90e428977563c351eeee7bcdb16dd9053e6ddaec
Pranava J K <jkpranava16@gmail.com>
2026-09-13 19:37:07 -0500

    build: local personal build config (own appId, updates from fork, no notarization)

--- src/updater.js
+++ src/updater.js
@@ -33,7 +33,7 @@ class UpdateManager {
 
     autoUpdater.setFeedURL({
       provider: "github",
-      owner: "OpenWhispr",
+      owner: "pranavajk1",
       repo: "openwhispr",
       private: false,
     });
